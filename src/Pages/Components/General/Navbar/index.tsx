aab4899cfdfe427a9ee79c44ec6de99102d09d45
Peregrinoo <gabriel.ribeiro07@aluno.ifce.edu.br>
2024-06-29 18:18:08 -0300

    :fire: [Navbar completed]

--- src/Pages/Components/General/Navbar/index.tsx
+++ src/Pages/Components/General/Navbar/index.tsx
@@ -3,14 +3,14 @@ import logo from './../../../../assets/logo_app.png'
 
 const NavBar = () => {
     return(
-            <HStack justify={'space-between'} bg={'brand.secundary-color'} p={'8px'}>
+            <HStack justify={'space-between'} bg={'brand.secundary-color'} p={'8px'} paddingRight={'32px'} paddingLeft={"32px"}>
 
-                <HStack>
-                    <Image src={logo} h={'60px'}/>
+                <HStack fontFamily={'brand.logoFont'}>
+                    <Image src={logo} h={'60px'} />
                     <Text fontSize={'22px'} color={'whitesmoke'}>Peregrino</Text>
                 </HStack>
 
-                <HStack spacing={'16px'}>
+                <HStack spacing={'16px'} fontFamily={"brand.navFont"}>
                     <Link> <Text color={'whitesmoke'}>CPS Test</Text> </Link>
                     <Link> <Text color={'whitesmoke'}>About me</Text> </Link>
                 </HStack>
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {ChakraBaseProvider, extendTheme} from "@chakra-ui/react";
 import MyRoute from "./route";
+import '@fontsource/roboto/500.css'
+import "@fontsource/days-one/400.css"; // Specify weight and style
+import '@fontsource/fredoka/500.css'
 
 const theme = extendTheme({
+    fonts: {
+        brand: {
+            "logoFont": `'Days One', sans-serif`,
+            "navFont": `'Fredoka', sans-serif`,
+            "bodyFont": `'Roboto', sans-serif`,
+        }
+    },
+
     colors: {
         brand: {
             'primary-color': "#232323",
